441fb07a5c5d46a2483bd502f9019ff8f76a9b55
g12935nr <g12935nr@s30436.tsuda.ac.jp>
2015-04-10 11:06:21 +0900

    HelloWorldです。

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstgit;
+
+/**
+ *
+ * @author g12935nr
+ */
+public class FirstGit {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          System.out.println("Hello World");
+    }
+}
